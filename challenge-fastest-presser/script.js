f303f76f7a72fc9ae6aea466054c9eb2e2968b77
mahri09 <63245237+mahri09@users.noreply.github.com>
2022-02-21 01:42:58 +0000

    challende-faster working with debugs have to be reviewed

--- challenge-fastest-presser/script.js
+++ challenge-fastest-presser/script.js
@@ -1,11 +1,48 @@
-function startGame() {}
+let scoreS = 0;
+let scoreL = 0;
+let numOfPressedL = document.createElement("p");
+let numOfPressedS = document.createElement("p");
+let inputVal = document.querySelector("input");
+const submitBTn = document.getElementById("submit");
+const parentS = document.getElementById("s-parent");
+const parentL = document.getElementById("l-parent");
 
-function keyBoardEvents(e) {
-  if (e.keyCode === 83) {
-    // On 'S' Pressed
-  } else if (e.keyCode === 76) {
-    // On 'L' Pressed
+console.log(parentL);
+
+function stop() {
+  if (parseInt(numOfPressedS.innerText) > parseInt(numOfPressedL.innerText)) {
+    numOfPressedS.innerText = "You Win!";
+    numOfPressedL.innerText = "You Failed";
+  } else {
+    numOfPressedS.innerText = "You Failed!";
+    numOfPressedL.innerText = "You Win";
   }
 }
 
+function startGame() {
+  numOfPressedS.innerText = parseInt(inputVal.value) + scoreS;
+  numOfPressedL.innerText = parseInt(inputVal.value) + scoreL;
+  parentL.appendChild(numOfPressedL);
+  parentS.appendChild(numOfPressedS);
+  let myTimeout = setTimeout(() => {
+    stop();
+    // alert("stop!");
+    clearTimeout(myTimeout);
+  }, 3000);
+}
+
+function keyBoardEvents(e) {
+  numOfPressedS.innerText = scoreS;
+  numOfPressedL.innerText = scoreL;
+  if (e.keyCode === 115) {
+    scoreS++;
+    console.log(scoreS);
+  } else if (e.keyCode === 108) {
+    scoreL++;
+    console.log(scoreL);
+  }
+  startGame();
+}
+
 document.addEventListener("keypress", keyBoardEvents);
+submitBTn.addEventListener("click", startGame);
